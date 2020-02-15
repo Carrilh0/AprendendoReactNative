9f5d8f1b134560da4351ca4afc361cb93ed2fd20
Vitor Carrilho <vitor.carrilho@hotmail.com>
2020-02-15 01:24:18 -0300

    melhoria no par ou impar

--- src/components/ParImpar.js
+++ src/components/ParImpar.js
@@ -1,22 +1,35 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {Component} from 'react';
+import {View, Text, TextInput} from 'react-native';
 import Padrao from '../estilo/padrao'
 
 
-const Teste = (numero) => {
-    
-    return numero % 2 == 0 ? <Text>Par</Text> : <Text>Impar</Text>;
+const Teste = (props) => {
+    return props.numero == 0 ? <Text>Insira um numero</Text> : props.numero % 2 == 0 ? <Text>Par</Text> : <Text>Impar</Text>;
 }
 
-const ParImpar = (props) => {
-    
+class ParImpar extends Component{
+
+    constructor(props){
+        super(props);
+        this.state = {
+            numero: 0,
+        }
+    }
+
+    alterarTexto = (numero) => {
+        this.setState({numero})
+    }
+
+    render(){
     return (
         <View>
             {
-                Teste(props.numero)
+                <Teste numero={this.state.numero}></Teste>
             }
+            <TextInput style={Padrao.ex} numeric value={`${this.state.numero}`} keyboardType={'numeric'} onChangeText={this.alterarTexto}></TextInput>
         </View>
     );
+    }
 }
 
 export default ParImpar;
